037647e9d541203c64a94a061c542c092da2e00c
Wojtek Swiderski <wswiders@uwaterloo.ca>
2016-09-07 23:45:12 -0700

    MongeEngine

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
 from flask import Flask
-from mongokat import Collection, Document
-
-class SampleDocument(Document):
-
-        def my_sum(self):
-                return self["a"] + self["b"]
-
-class SampleCollection(Collection):
-        document_class = SampleDocument
-
-        def find_by_a(self, a_value):
-                return self.find_one({"a": a_value})
-
-# Then use it in your code like this:
-
-from pymongo import MongoClient
-client = MongoClient()
-Sample = SampleCollection(collection=client.my_db.my_col)
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+db = MongoEngine(app)
 
 @app.route("/")
 def hello():
-    Sample.insert({"a": 1, "b": 2})
-    five = Sample.find_by_a(1)
-    return str(five.my_sum())
+    return "Hello world!"
 
 if __name__ == "__main__":
     app.run()
